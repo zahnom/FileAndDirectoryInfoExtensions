1dad92d7957007dfb2492014db3c6ec36518d4eb
Marc Zahno <marc.zahno@ypsomed.com>
2020-02-26 14:00:38 +0100

    Added CopyContentTo and CreateFile extensions

---
+++ ExtensionTests/ExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FileAndDirectoryInfoExtensionsNamespace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileAndDirectoryInfoExtensionsTestsNamespace
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        public TestContext TestContext { get; set; }
+        public DirectoryInfo TestDirectory => new DirectoryInfo(TestContext.TestRunDirectory);
+        public DirectoryInfo DirWithOriginalData;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            DirWithOriginalData = TestDirectory
+                .CreateSubdirectory("DirectoryWithOriginals");
+
+            DirWithOriginalData.CreateFile("TestFile1.txt");
+            DirWithOriginalData.CreateFile("TestFile2.txt");
+            DirWithOriginalData.CreateSubdirectory("SomeSubdir")
+                .CreateFile("TestFile1InSubdir.txt");
+        }
+
+        [TestMethod]
+        public void TestCopyContent()
+        {
+            var dirWithCopiedData = TestDirectory
+                .CreateSubdirectory("DirectoryWithCopies");
+
+            DirWithOriginalData.CopyContentTo(dirWithCopiedData);
+
+            var copiedFiles = Directory
+                .GetFiles(dirWithCopiedData.FullName, "*.*", SearchOption.AllDirectories)
+                .Select(s => new FileInfo(s).Name)
+                .ToList();
+            var copiedDirectories = Directory
+                .GetDirectories(dirWithCopiedData.FullName)
+                .Select(s => new DirectoryInfo(s).Name)
+                .ToList();
+
+            Assert.IsTrue(copiedFiles.Contains("TestFile1.txt"));
+            Assert.IsTrue(copiedFiles.Contains("TestFile2.txt"));
+            Assert.IsTrue(copiedFiles.Contains("TestFile1InSubdir.txt"));
+            Assert.AreEqual(3, copiedFiles.Count);
+
+            Assert.IsTrue(copiedDirectories.Contains("SomeSubdir"));
+            Assert.AreEqual(1, copiedDirectories.Count);
+        }
+    }
+}
---
+++ ExtensionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExtensionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ExtensionTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b6aea46-b7d3-4063-9aef-955f7147ec20")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
